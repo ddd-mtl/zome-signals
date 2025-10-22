5e8bc291b23223ea141eb0c7f126ee653a043338
ddd-mtl <doute.d@gmail.com>
2025-10-22 15:03:21 -0400

    0.6.0

--- src/callbacks/init.rs
+++ src/callbacks/init.rs
@@ -2,11 +2,11 @@ use hdk::capability::create_cap_grant;
 use hdk::info::zome_info;
 use hdk::map_extern::ExternResult;
 use hdk::prelude::{ActionHash, CapAccess, CapGrantEntry, FunctionName, GrantedFunctions};
-use std::collections::BTreeSet;
+use std::collections::HashSet;
 
 ///
 pub fn create_signal_cap_grant() -> ExternResult<ActionHash> {
-   let mut fns = BTreeSet::new();
+   let mut fns = HashSet::new();
    fns.insert((zome_info()?.name, FunctionName("recv_remote_signal".into())));
    let cap_grant_entry: CapGrantEntry = CapGrantEntry::new(
       String::from("recv_remote_signal"),
